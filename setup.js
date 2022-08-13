a0a6947dcc281fe33575c804a8e59f4b08fb5ef0
Rez <77873738+rezcoco@users.noreply.github.com>
2022-08-13 21:07:33 +0700

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,12 @@
+const { writeFile } = require('node:fs/promises')
+const axios = require('axios')
+const GD_TOKEN = process.env.GD_TOKEN
+const CREDENTIALS = process.env.CREDENTIALS
+
+(async function() {
+  const token = await axios.get('GD_TOKEN');
+  const credentials = await axios.get('CREDENTIALS')
+  
+  await writeFile(__dirname+'/credentials.json', JSON.stringify(credentials.data))
+  await writeFile(__dirname+'/token.json', JSON.stringify(token.data))
+})();
