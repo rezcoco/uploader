cea6e1a0d9779aa772805fba9ae991c46b42afe9
Rez <77873738+rezcoco@users.noreply.github.com>
2022-08-14 16:14:33 +0700

    Add files via upload

--- src/drive/driveAuth.js
+++ src/drive/driveAuth.js
@@ -1,10 +1,16 @@
-const { readFile } = require('node:fs/promises');
+require('dotenv').config();
 const {google} = require('googleapis');
+const axios = require('axios')
+const GD_TOKEN = process.env.GD_TOKEN
+const CREDENTIALS = process.env.CREDENTIALS
 
 async function gdriveAuth() {
   const SCOPES = ['https://www.googleapis.com/auth/drive'];
-  const credentials = await readFile('../../credentials.json')
-  const token = await readFile('../../token.json')
+  const t = await axios.get('GD_TOKEN');
+  const c = await axios.get('CREDENTIALS')
+  const credentials = JSON.stringify(c.data)
+  const token = JSON.stringify(t.data)
+  
   const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
   oAuth2Client.setCredentials(token)
