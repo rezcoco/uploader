1518b85ab8769411fffd69d7932aedd9e4c48646
Rez <77873738+rezcoco@users.noreply.github.com>
2022-08-15 08:20:44 +0700

    Add files via upload

--- src/drive/gdriveTools.js
+++ src/drive/gdriveTools.js
@@ -5,14 +5,12 @@ const mime = require('mime');
 const { google } = require('googleapis');
 const { gdriveAuth } = require('./driveAuth')
 const { GdriveDownloadStatus, downloadStatus } = require('../dlStatus')
-const { download_list, interval } = require('../utils')
 const teamDriveID = process.env.TD_ID
 
 
 async function upload(fileName, filePath, gid) {
   const auth = await gdriveAuth()
   const drive = google.drive({ version: 'v3', auth });
-  const f = await stat(filePath)
   
   const media = {
     body: createReadStream(filePath),
@@ -28,16 +26,7 @@ async function upload(fileName, filePath, gid) {
         description: 'Uploded by rezcoco'
       },
       media
-    }, {
-      onUploadProgress: function(c) {
-        const completed = Number(c.bytesRead.toString())
-        download_list[gid] = new GdriveDownloadStatus(fileName, downloadStatus['STATUS_UPLOADING'], f.size, completed)
-        
-      }
     });
-    const intervalId = interval.findIndex((i) => i == gid )
-    interval.splice(intervalId, 1)
-    delete download_list[gid]
     
     await drive.permissions.create({
       fileId: file.data.id,
