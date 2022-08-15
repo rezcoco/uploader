20d10194640bd7f7dd55e53eaea7d109d89919da
Rez <77873738+rezcoco@users.noreply.github.com>
2022-08-15 07:44:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ wget 'https://www.rarlab.com/rar/rarlinux-x64-612.tar.gz'
 tar -zxvf rarlinux-x64-612.tar.gz
 cd rar
 cp -v rar unrar /usr/local/bin/
-cd && rm -rf rar 'rarlinux-x64-612.tar.gz'
+cd && rm -rf 'rarlinux-x64-612.tar.gz'
