02f29b4f6deffdd7528abf1b78e838e98e5d612e
Rez <77873738+rezcoco@users.noreply.github.com>
2022-08-27 13:22:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -174,6 +174,7 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
 process.on('exit', () => {
   ariaTools.stop()
+  clean(__dirname+/'downloads')
 });
 
 bot.onText(/\/upload (.+)/, uploadCmdHandler)
