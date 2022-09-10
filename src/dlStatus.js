743b1e6c2036a9180585159ef16c2dd12a777f83
rezcoco <rezaainulzaki23@gmail.com>
2022-09-10 22:40:13 +0700

    removing console shit

--- src/dlStatus.js
+++ src/dlStatus.js
@@ -87,7 +87,6 @@ function formatNumber (n) {
 function getFileName(filePath) {
   const { path } = filePath
   const r = path.match(/(?=downloads\/\d+\/).+/)
-  console.log(r)
   if (r) {
     return r[0].split('/')[2]
   }
