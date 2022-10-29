0528c9c92214cabb16248cfd738b52f11e46ae6e
rezcoco <rezaainulzaki23@gmail.com>
2022-10-29 22:47:24 +0700

    fixup! update fix

--- src/dl.js
+++ src/dl.js
@@ -43,7 +43,7 @@ class AriaTools {
     async addDownload (url, dir) {
         try {
             const uri = await url
-            return this.aria2.call('addUri', [uri], { dir: path.join(__dirname, 'downloads', String(dir)), 'content-disposition-default-utf8': 'true', '-auto-file-renaming': 'false' })
+            return this.aria2.call('addUri', [uri], { dir: path.join(__dirname, 'downloads', String(dir)), 'content-disposition-default-utf8': 'true', 'auto-file-renaming': 'false' })
         } catch (e) {
             console.log(e)
         }
